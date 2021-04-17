3e476de2965b66789ebfb5bbd7c1a6ae0bb17f31
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2021-04-17 18:12:29 +0100

    Added loop to query all pages.

--- src/main/java/com/marvel/api/marvelapi/DatabasePopulator.java
+++ src/main/java/com/marvel/api/marvelapi/DatabasePopulator.java
@@ -1,6 +1,7 @@
 package com.marvel.api.marvelapi;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.marvel.api.marvelapi.model.Data;
 import com.marvel.api.marvelapi.model.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,24 +22,35 @@ class DatabasePopulator implements CommandLineRunner {
     private String hash;
     @Value("${marvel.api.publicKey}")
     private String publicKey;
+    private final int LIMIT = 100;
 
     private static final Logger logger = LoggerFactory.getLogger(DatabasePopulator.class);
 
     @Override
     public void run(String... args) throws Exception {
-        logger.info("Populating DB with character ids taken from " + baseURL);
-        HttpClient client = HttpClient.newHttpClient();
-        String url = baseURL +
-                "/v1/public/characters?limit=100&offset=1&ts=1&apikey=" +
-                publicKey +
-                "&hash=" +
-                hash;
-        HttpRequest request = HttpRequest.newBuilder(URI.create(url))
-                .header("accept", "application/json")
-                .build();
-        HttpResponse<String> stringResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        Response response = new ObjectMapper().readValue(stringResponse.body(), Response.class);
-        System.out.println(response.data.results.get(0).id);
+        logger.info("Populating the DB with character ids taken from " + baseURL + "...");
+        int offset = 0;
+        int total = 1;
+        while (offset < total) {
+            String url = baseURL +
+                    "/v1/public/characters?limit=" +
+                    LIMIT +
+                    "&offset=" +
+                    offset +
+                    "&ts=1&apikey=" +
+                    publicKey +
+                    "&hash=" +
+                    hash;
+            HttpRequest request = HttpRequest.newBuilder(URI.create(url))
+                    .header("accept", "application/json")
+                    .build();
+            HttpResponse<String> stringResponse = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            Response response = new ObjectMapper().readValue(stringResponse.body(), Response.class);
+            Data data = response.data;
+            offset += data.count;
+            total = data.total;
+        }
+        logger.info("Finished populating the DB with " + offset + " character ids.");
     }
 
 }
